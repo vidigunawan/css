95435546a26286224efac7866944e5b0d6316b63
vidigunawan <37113305+vidigunawan@users.noreply.github.com>
2018-03-12 21:56:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="Asset/css/style.css"/>
+        <link rel="stylesheet" href="Assets/CSS/bootstrap.css">
+        
+        
+    </head>  
+    <body>
+        <form action="login.php" method="POST">
+            
+		<div class="box">
+                    <h1>Login</h1>
+                    <input name="Email"  type="email" id="inEmail" placeholder="Email Address" autofocus="yes" autocomplete="no" accept="" required="yes">
+                    <input name="Pass"   type="password" accept="" id="inPass" placeholder="Passowrd" required="yes">
+                   
+                    <button  type="submit">Sign in</button>
+                     <p type="checkbox">Remember me ?</p>
+                </div>
+        </form>
+        
+    </body>
+</html>
